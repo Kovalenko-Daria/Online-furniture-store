d2d993e22599bd0afe48f9865be913920fd2e3ab
Daria <dkovalenko25@mail.ru>
2024-04-08 21:18:49 +0300

    profile added

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -18,4 +18,15 @@ class UserRegistrationForm(UserCreationForm):
             'email',
             'password1',
             'password2',
+        )
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            'image',
+            'first_name',
+            'last_name',
+            'username',
+            'email',
         )
